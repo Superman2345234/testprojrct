fe79837dbda12fed57e799e62173875fbd7734d4
spannjrl <spannjrl@IVANOVA.ecs.csus.edu>
2018-10-26 12:51:20 -0700

    first comment

--- testprojct/src/testprojct/Hello.java
+++ testprojct/src/testprojct/Hello.java
@@ -1,5 +1,21 @@
 package testprojct;
-
 public class Hello {
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("SacStateID - This line was added by Lonnie Spann Jr.");
+		System.out.println("SacStateID - This line was added by Micheal Dorst.");
+		System.out.println("SacStateID - This line was added by Daivd Liu.");
+		System.out.println("SacStateID - This line was added by Timonthy darrow .");
+		System.out.println("SacStateID - This line was added by David ray .");
+		System.out.println("SacStateID - This line was added by Daniel Kuzmin.");
+		System.out.println("SacStateID - This line was added by Ruth Chow .");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
 }
+
+}
+
